434b9d9a73ef82c0470e886193d9127304214104
jacqueline <jacqueline_rwa@msn.com>
2023-12-02 20:06:27 +0100

    refactor(day2): Your code can only have one level of indentation

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -1,27 +1,55 @@
 package games;
 
 public class FizzBuzz {
+
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final int FIZZ_NUMBER = 3;
+    private static final int BUZZ_NUMBER = 5;
+    private static final int MAX_RANGE = 100;
+    private static final int MIN_RANGE = 0;
+
     private FizzBuzz() {
     }
-    
-    public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+
+    public static String convert(Integer number) throws OutOfRangeException {
+        if (isOutOfRange(number)) {
             throw new OutOfRangeException();
         }
+
+        if (isDivisibleByFizzAndBuzzNumbers(number)) {
+            return FIZZ + BUZZ;
+        }
+
+        if (isDivisibleByFizzNumber(number)) {
+            return FIZZ;
+        }
+
+        if (isDivisibleByBuzzNumber(number)) {
+            return BUZZ;
+        }
+
+        return number.toString();
     }
+
+    private static boolean isOutOfRange(Integer number) {
+        return number <= MIN_RANGE || number > MAX_RANGE;
+    }
+
+    private static boolean isDivisibleByFizzAndBuzzNumbers(Integer number) {
+        return isDivisibleByFizzNumber(number) && isDivisibleByBuzzNumber(number);
+    }
+
+    private static boolean isDivisibleByFizzNumber(Integer number) {
+        return isDivisibleBy(number, FIZZ_NUMBER);
+    }
+
+    private static boolean isDivisibleByBuzzNumber(Integer number) {
+        return isDivisibleBy(number, BUZZ_NUMBER);
+    }
+
+    private static boolean isDivisibleBy(Integer number, int divisor) {
+        return number % divisor == 0;
+    }
+
 }
